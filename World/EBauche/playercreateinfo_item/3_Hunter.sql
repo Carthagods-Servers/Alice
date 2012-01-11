9a99cf6e8d1ed78147582feda04a00150e78b2ad
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-11 23:26:35 +0100

    Correction des chasseur complété!

---
+++ World/EBauche/playercreateinfo_item/3_Hunter.sql
@@ -0,0 +1,61 @@
+-- supression des objets prédifinie 
+INSERT INTO `playercreateinfo_item` VALUES ('0', '3', '12282', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '0', '51809', '-1');
+-- Dwarf Hunter
+INSERT INTO `playercreateinfo_item` VALUES ('3', '3', '148', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('3', '3', '147', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('3', '3', '129', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('3', '3', '2508', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('3', '3', '2516', '-200');
+INSERT INTO `playercreateinfo_item` VALUES ('3', '3', '2102', '-1');
+-- Night Elf Hunter
+INSERT INTO `playercreateinfo_item` VALUES ('4', '3', '148', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('4', '3', '147', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('4', '3', '129', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('4', '3', '2504', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('4', '3', '2512', '-200');
+INSERT INTO `playercreateinfo_item` VALUES ('4', '3', '2101', '-1');
+-- Draenei Hunter
+INSERT INTO `playercreateinfo_item` VALUES ('11', '3', '23345', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('11', '3', '23344', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('11', '3', '23348', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('11', '3', '12247', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('11', '3', '2512', '-200');
+INSERT INTO `playercreateinfo_item` VALUES ('11', '3', '2101', '-1');
+-- Orc Hunter
+INSERT INTO `playercreateinfo_item` VALUES ('2', '3', '127', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('2', '3', '6126', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('2', '3', '6127', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('2', '3', '2504', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('2', '3', '2512', '-200');
+INSERT INTO `playercreateinfo_item` VALUES ('2', '3', '2101', '-1');
+-- Tauren Hunter
+INSERT INTO `playercreateinfo_item` VALUES ('6', '3', '127', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('6', '3', '6126', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('6', '3', '2508', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('6', '3', '2516', '-200');
+INSERT INTO `playercreateinfo_item` VALUES ('6', '3', '2102', '-1');
+-- Blood Elf Hunter
+INSERT INTO `playercreateinfo_item` VALUES ('10', '3', '20901', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('10', '3', '20899', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('10', '3', '20900', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('10', '3', '20980', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('10', '3', '2512', '-200');
+INSERT INTO `playercreateinfo_item` VALUES ('10', '3', '2101', '-1');
+-- TROOOOOOL HUNTER FTW
+INSERT INTO `playercreateinfo_item` VALUES ('8', '3', '-6126', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('8', '3', '-2504', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('8', '3', '2512', '-200');
+INSERT INTO `playercreateinfo_item` VALUES ('8', '3', '2101', '-1');
+
+-- Ajout de S7 Chasseur Parure de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '3', '41088', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '3', '41144', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '3', '41158', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '3', '41206', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '3', '41218', '1');
+
+-- Ajout de Hors Set
+INSERT INTO `playercreateinfo_item` VALUES ('0','3','41226','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','3','41236','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','3','41231','1');
