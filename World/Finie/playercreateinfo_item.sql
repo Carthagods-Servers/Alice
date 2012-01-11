42656256af0b9db1b38181cbdef6a6a5cbb66b6b
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-11 23:32:54 +0100

    Ajout de correction pour la classe Paladin

--- World/Finie/playercreateinfo_item.sql
+++ World/Finie/playercreateinfo_item.sql
@@ -65,3 +65,46 @@ INSERT INTO `playercreateinfo_item` VALUES ('0','1','40884','1');
 INSERT INTO `playercreateinfo_item` VALUES ('0','1','40883','1');
 INSERT INTO `playercreateinfo_item` VALUES ('0','1','40890','1');
 
+
+
+-- supression des objets prédifinie 
+-- Human Paladin
+INSERT INTO `playercreateinfo_item` VALUES ('1', '2', '45', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('1', '2', '2361', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('1', '2', '43', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('1', '2', '44', '-1'); 
+-- Dwarf Paladin
+INSERT INTO `playercreateinfo_item` VALUES ('3', '2', '6117', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('3', '2', '2361', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('3', '2', '6118', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('3', '2', '43', '-1');
+-- Draenei Paladin
+INSERT INTO `playercreateinfo_item` VALUES ('11', '2', '23473', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('11', '2', '23346', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('11', '2', '23474', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('11', '2', '23475', '-1');
+-- Blood Elf Paladin
+INSERT INTO `playercreateinfo_item` VALUES ('10', '2', '23346', '-1');
+
+-- Ajout de S7 Paladin Justification de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40792', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40812', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40831', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40852', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40872', '1');
+-- Ajout de S7 Paladin Rédemption de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40910', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40928', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40934', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40940', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40964', '1');
+
+-- Ajout de Hors Set
+INSERT INTO `playercreateinfo_item` VALUES ('0','2','40890','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','2','40883','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','2','40884','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','2','40984','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','2','40978','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','2','40979','1');
+
+
