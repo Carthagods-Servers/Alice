675e4cbbd89a617b03a6bfffe7274200191ee993
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-11 23:31:48 +0100

    Ajout de correction pour la Classe Guerrier

--- World/Finie/playercreateinfo_item.sql
+++ World/Finie/playercreateinfo_item.sql
@@ -6,3 +6,62 @@ CREATE TABLE `playercreateinfo_item` (
   `amount` tinyint(3) NOT NULL DEFAULT '1',
   KEY `playercreateinfo_race_class_index` (`race`,`class`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- supression des objets prédifinie 
+-- Human Warrior
+INSERT INTO `playercreateinfo_item` VALUES ('1', '1', '39', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('1', '1', '40', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('1', '1', '38', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('1', '1', '49778', '-1'); 
+-- Dwarf Warrior
+INSERT INTO `playercreateinfo_item` VALUES ('3', '1', '38', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('3', '1', '12282', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('3', '1', '39', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('3', '1', '40', '-1'); 
+-- Night Elf Warrior
+INSERT INTO `playercreateinfo_item` VALUES ('4', '1', '6120', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('4', '1', '6121', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('4', '1', '6122', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('4', '1', '49778', '-1');
+-- Gnome Warrior
+INSERT INTO `playercreateinfo_item` VALUES ('7', '1', '38', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('7', '1', '39', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('7', '1', '40', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('7', '1', '49778', '-1');
+-- Draenei Warrior
+INSERT INTO `playercreateinfo_item` VALUES ('11', '1', '23473', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('11', '1', '23346', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('11', '1', '23474', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('11', '1', '23475', '-1');
+-- Orc Warrior
+INSERT INTO `playercreateinfo_item` VALUES ('2', '1', '6125', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('2', '1', '12282', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('2', '1', '139', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('2', '1', '140', '-1');
+-- Undead Warrior
+INSERT INTO `playercreateinfo_item` VALUES ('5', '1', '6125', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('5', '1', '49778', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('5', '1', '139', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('5', '1', '140', '-1');
+-- Tauren Warrior
+INSERT INTO `playercreateinfo_item` VALUES ('6', '1', '6125', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('6', '1', '2361', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('6', '1', '139', '-1'); 
+-- Troll Warrior
+INSERT INTO `playercreateinfo_item` VALUES ('8', '1', '6125', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('8', '1', '25861', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('8', '1', '139', '-1'); 
+INSERT INTO `playercreateinfo_item` VALUES ('8', '1', '49778', '-1');
+
+-- Ajout de S7 Gerrier gladiateur implacable en plaques
+INSERT INTO `playercreateinfo_item` VALUES ('0', '1', '40790', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '1', '40810', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '1', '40829', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '1', '40850', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '1', '40870', '1');
+
+-- Ajout de Hors Set
+INSERT INTO `playercreateinfo_item` VALUES ('0','1','40884','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','1','40883','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','1','40890','1');
+
