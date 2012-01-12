fbea2bc21946d8b40e892d1d0107a2a702a2bf78
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-12 14:13:04 +0100

    Update World/EBauche/playercreateinfo_item/8_Mage.sql

---
+++ World/EBauche/playercreateinfo_item/8_Mage.sql
@@ -0,0 +1,18 @@
+-- S7 Mage Tenue de parade de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '8', '41947', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '8', '41954', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '8', '41960', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '8', '41966', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '8', '41972', '1');
+
+-- HS Mage
+INSERT INTO Playercreateinfo_item (race,class,itemid,amount) VALUES
+('0','8','41910','1'),
+('0','8','41899','1'),
+('0','8','41904','1'),
+('0','8','41894','1'),
+('0','8','41882','1'),
+('0','8','41886','1'),
+('0','8','49181','1'),
+('0','8','49179','1'),
+('0','8','49183','1');
