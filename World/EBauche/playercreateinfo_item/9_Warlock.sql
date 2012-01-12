168d346ec37c7a080c7b33869e456c7843d0e135
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-12 14:14:15 +0100

    Update World/EBauche/playercreateinfo_item/9_Warlock.sql

---
+++ World/EBauche/playercreateinfo_item/9_Warlock.sql
@@ -0,0 +1,18 @@
+-- S7 Démoniste Suaire gangréné de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '9', '41994', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '9', '41999', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '9', '42006', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '9', '42012', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '9', '42018', '1');
+
+-- HS Warlock
+INSERT INTO Playercreateinfo_item (race,class,itemid,amount) VALUES
+('0','9','41910','1'),
+('0','9','41899','1'),
+('0','9','41904','1'),
+('0','9','41894','1'),
+('0','9','41882','1'),
+('0','9','41886','1'),
+('0','9','49181','1'),
+('0','9','49179','1'),
+('0','9','49183','1');
