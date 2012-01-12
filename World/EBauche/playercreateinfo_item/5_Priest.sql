65fa97c82d1dd78a151165e277b3ca3483b0a5e3
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-12 14:09:07 +0100

    ajout de set+horsset prestS7

---
+++ World/EBauche/playercreateinfo_item/5_Priest.sql
@@ -0,0 +1,18 @@
+-- S7 Prêtre Grande tenue de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41916', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41922', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41928', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41935', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41941', '1');
+
+-- HS Priest
+INSERT INTO Playercreateinfo_item (race,class,itemid,amount) VALUES
+('0','5','41910','1'),
+('0','5','41899','1'),
+('0','5','41904','1'),
+('0','5','41894','1'),
+('0','5','41882','1'),
+('0','5','41886','1'),
+('0','5','49181','1'),
+('0','5','49179','1'),
+('0','5','49183','1');
