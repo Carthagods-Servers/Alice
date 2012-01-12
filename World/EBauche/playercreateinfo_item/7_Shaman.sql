59c9f70b0bf48b57ffa7e2610896fa01a59d01b5
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-12 14:10:49 +0100

    ajout de set + hors set chamanS7

---
+++ World/EBauche/playercreateinfo_item/7_Shaman.sql
@@ -0,0 +1,18 @@
+-- S7 chaman Tonnepoing de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '40995', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41008', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41020', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41034', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41045', '1');
+
+-- HS Chaman
+INSERT INTO Playercreateinfo_item (race,class,itemid,amount) VALUES
+('0','7','41066','1'),
+('0','7','41071','1'),
+('0','7','41076','1'),
+('0','7','41061','1'),
+('0','7','41052','1'),
+('0','7','41056','1'),
+('0','7','41226','1'),
+('0','7','41236','1'),
+('0','7','41231','1');
