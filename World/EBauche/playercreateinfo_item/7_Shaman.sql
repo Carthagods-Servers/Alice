6e42a03f989cb112b2d5eba3245d16247cfe034f
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-12 14:13:37 +0100

    Update World/EBauche/playercreateinfo_item/7_Shaman.sql

--- World/EBauche/playercreateinfo_item/7_Shaman.sql
+++ World/EBauche/playercreateinfo_item/7_Shaman.sql
@@ -5,6 +5,21 @@ INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41020', '1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41034', '1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41045', '1');
 
+-- S7 chaman Trembleterre de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41082', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41138', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41152', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41200', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41212', '1');
+
+-- S7 chaman Vague guerrière de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '40994', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41002', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41014', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41028', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41039', '1');
+
+
 -- HS Chaman
 INSERT INTO Playercreateinfo_item (race,class,itemid,amount) VALUES
 ('0','7','41066','1'),
