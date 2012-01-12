db631208bd599a1d0f756a45c7e11a437efe6d08
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-12 14:08:06 +0100

    ajout de set+hors set vovo S7

---
+++ World/EBauche/playercreateinfo_item/4_Rogue.sql
@@ -0,0 +1,11 @@
+-- S7 Voleur Habits de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '4', '41651', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '4', '41656', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '4', '41673', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '4', '41684', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '4', '41768', '1');
+-- HS Voleur
+INSERT INTO Playercreateinfo_item (race,class,itemid,amount) VALUES
+('0','4','41841','1'),
+('0','4','41833','1'),
+('0','4','41837','1');
