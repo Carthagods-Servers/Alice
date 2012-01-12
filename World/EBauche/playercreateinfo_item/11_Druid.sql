2a9597803468705a86056a06014b0033128aa7c3
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-12 14:14:51 +0100

    Update World/EBauche/playercreateinfo_item/11_Druid.sql

---
+++ World/EBauche/playercreateinfo_item/11_Druid.sql
@@ -0,0 +1,32 @@
+-- S7 Deruid Peau de fauve de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41282', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41294', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41305', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41317', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41328', '1');
+
+-- S7 Deruid Refuge de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41276', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41288', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41299', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41311', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41322', '1');
+
+-- S7 Deruid Sanctuaire de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41662', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41668', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41679', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41716', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41774', '1');
+
+-- HS Druid
+INSERT INTO Playercreateinfo_item (race,class,itemid,amount) VALUES
+('0','11','41641','1'),
+('0','11','41631','1'),
+('0','11','41636','1'),
+('0','11','41626','1'),
+('0','11','41618','1'),
+('0','11','41622','1'),
+('0','11','41841','1'),
+('0','11','41833','1'),
+('0','11','41837','1');
