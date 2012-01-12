e65c6dc04c2d763d381ff9dedde025955a8ffd3b
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-12 14:10:01 +0100

    ajout de set + hors set dk s7

---
+++ World/EBauche/playercreateinfo_item/6_Death Knight.sql
@@ -0,0 +1,12 @@
+-- S7 Prêtre Investiture de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41855', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41860', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41865', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41870', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41875', '1');
+
+-- HS DK
+INSERT INTO Playercreateinfo_item (race,class,itemid,amount) VALUES
+('0','6','40884','1'),
+('0','6','40883','1'),
+('0','6','40890','1');
