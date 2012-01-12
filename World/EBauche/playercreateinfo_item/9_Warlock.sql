24c230ada8f744125544ec35d4b8d20112816e0f
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-12 14:55:08 +0100

    correction termier pour les warlock

--- World/EBauche/playercreateinfo_item/9_Warlock.sql
+++ World/EBauche/playercreateinfo_item/9_Warlock.sql
@@ -1,3 +1,32 @@
+-- supression des objets prédifinie Warlock 
+-- Human Warlock
+INSERT INTO `playercreateinfo_item` VALUES ('1', '9', '35', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('1', '9', '57', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('1', '9', '6097', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('1', '9', '1396', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('1', '9', '59', '1');
+-- Gnome Warlock
+INSERT INTO `playercreateinfo_item` VALUES ('7', '9', '35', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('7', '9', '57', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('7', '9', '6097', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('7', '9', '1396', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('7', '9', '59', '1');
+-- Orc Warlock
+INSERT INTO `playercreateinfo_item` VALUES ('2', '9', '35', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('2', '9', '6129', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('2', '9', '1396', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('2', '9', '59', '1');
+-- Undead Warlock
+INSERT INTO `playercreateinfo_item` VALUES ('2', '9', '35', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('2', '9', '6129', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('2', '9', '1396', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('2', '9', '59', '1');
+-- Blood Elf Warlock
+INSERT INTO `playercreateinfo_item` VALUES ('10', '9', '20978', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('10', '9', '20892', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('10', '9', '6097', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('2', '9', '1396', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('10', '9', '59', '1');
 -- S7 Démoniste Suaire gangréné de gladiateur implacable
 INSERT INTO `playercreateinfo_item` VALUES ('0', '9', '41994', '1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '9', '41999', '1');
@@ -6,13 +35,12 @@ INSERT INTO `playercreateinfo_item` VALUES ('0', '9', '42012', '1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '9', '42018', '1');
 
 -- HS Warlock
-INSERT INTO Playercreateinfo_item (race,class,itemid,amount) VALUES
-('0','9','41910','1'),
-('0','9','41899','1'),
-('0','9','41904','1'),
-('0','9','41894','1'),
-('0','9','41882','1'),
-('0','9','41886','1'),
-('0','9','49181','1'),
-('0','9','49179','1'),
-('0','9','49183','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','9','41910','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','9','41899','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','9','41904','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','9','41894','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','9','41882','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','9','41886','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','9','49181','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','9','49179','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','9','49183','1');
