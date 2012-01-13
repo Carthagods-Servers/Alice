d47ec5106256868afc435874c4a1f007f01c3e58
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-13 20:53:17 +0100

    [Druid] Ajout de Set+HS S7 & Suppression de stuff prédéfinie

--- World/Finie/playercreateinfo_item.sql
+++ World/Finie/playercreateinfo_item.sql
@@ -456,3 +456,44 @@ INSERT INTO `playercreateinfo_item` VALUES ('0', '8', '41886', '1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '8', '49181', '1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '8', '49179', '1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '8', '49183', '1');
+
+
+-- Night Elfe Druid
+INSERT INTO `playercreateinfo_item` VALUES ('4', '11', '6123', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('4', '11', '6124', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('4', '11', '3661', '-1');
+-- Tauren Druid
+INSERT INTO `playercreateinfo_item` VALUES ('6', '11', '6139', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('6', '11', '6124', '-1');
+INSERT INTO `playercreateinfo_item` VALUES ('6', '11', '35', '-1');
+-- S7 Deruid Peau de fauve de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41282', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41294', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41305', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41317', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41328', '1');
+-- S7 Deruid Refuge de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41276', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41288', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41299', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41311', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41322', '1');
+-- S7 Deruid Sanctuaire de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41662', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41668', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41679', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41716', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41774', '1');
+-- HS Druid
+INSERT INTO `playercreateinfo_item` VALUES ('0','11','41641','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','11','41631','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','11','41636','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','11','41626','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','11','41618','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','11','41622','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','11','41841','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','11','41833','1');
+INSERT INTO `playercreateinfo_item` VALUES ('0','11','41837','1');
+
+
+
